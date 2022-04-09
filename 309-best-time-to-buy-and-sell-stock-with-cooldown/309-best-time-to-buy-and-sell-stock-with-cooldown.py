2dd4469c8d8453fa24c2e00c138ad154f306eedb
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-04-09 11:05:17 +0300

    Time: 5851 ms (5.00%), Space: 15.9 MB (33.32%) - LeetHub

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        memo = {}
+        ans = 0
+        
+        def dfs(buy):
+            """ buy and sell for a given stock
+            """
+            # check buy is in index
+            if buy >= len(prices):
+                return 0
+            
+            if buy in memo:
+                # print(buy, memo[buy])
+                return memo[buy]
+            
+            ans = profit = 0 
+            for sell in range(buy + 1, len(prices)):
+                profit = max(prices[sell] - prices[buy], profit)
+                ans = max(profit + dfs(sell + 2), ans)
+                # print(buy, sell, profit, ans, dfs(sell + 2))
+            
+            memo[buy] = ans
+            return ans
+        
+        for i in range(len(prices)):
+            ans = max(ans, dfs(i))
+        
+        return ans
+        
