b5d80927521a2d0b86786d9c21e9d6fc5b5fbd5e
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-05-12 02:22:20 +0300

    Time: 7120 ms (5.05%), Space: 13.7 MB (96.24%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
@@ -1,35 +1,18 @@
 class Solution:
     def countVowelStrings(self, n: int) -> int:
-        if n == 50:
-            return 316251
-        if n == 49:
-            return 292825
-        ans = []
-        """
-        aaa aae aee 
-        eee eei eii
-        
-        """
         ans = 0
         let = ["a","e","i","o","u"]
         siz = len(let)
 
             
-        def dfs(temp, curr, num):
-            nonlocal ans, siz, n
-            if num == n:
+        def dfs(curr, num):
+            nonlocal ans, siz
+            if num == 0:
                 ans += 1
                 return
-            
+
             for i in range(curr, siz):
-                temp.append(let[i])
-                dfs(temp, i, num + 1)
-                temp.pop()
+                dfs(i, num - 1)
         
-        dfs([], 0, 0)
-        # return len(ans)
+        dfs( 0, n)
         return ans
-            
-#         for i in range(len(let)):
-#             for j in range(len(let)):
-                
