a3eb7a32fb097ad854bd0aef1d515ef25b0793f5
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-05-13 17:33:57 +0300

    Time: 51 ms (30.90%), Space: 13.9 MB (18.51%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
@@ -8,16 +8,5 @@ class Solution:
         1 0 0 0 0 
         """
         count = 0
-        num1 = list(reversed(bin(start)[2:]))
-        num2 = list(reversed(bin(goal)[2:]))
-
-        diff = abs(len(num1) - len(num2))
-        
-        num1 = num1 + ["0"] * diff
-        num2 =  num2 + ["0"] * diff
-        
-        for i in range(min(len(num1), len(num2))):
-            if int(num1[i]) ^ int(num2[i]):
-                count += 1
-        
-        return count
+        num = bin(start ^ goal)
+        return num.count("1")
