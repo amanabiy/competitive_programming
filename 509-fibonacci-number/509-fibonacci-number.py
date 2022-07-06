8ed581f364c9d0bda5f3f7bbabba01dbc8c6fb7d
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-07-06 09:46:24 +0300

    Time: 40 ms (75.59%), Space: 13.9 MB (9.50%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,7 +1,11 @@
+memory = {}
 class Solution:
     def fib(self, n: int) -> int:
+        if n in memory:
+            return memory[n]
         if n == 0:
             return 0
         if n == 1:
             return 1
-        return self.fib(n-1) + self.fib(n-2)
+        memory[n] = self.fib(n-1) + self.fib(n-2)
+        return memory[n]
