218b233704d5acc2b4e4ad7de537cf908c9ba701
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-07-28 20:04:36 +0300

    Time: 54 ms (53.74%), Space: 13.9 MB (69.31%) - LeetHub

--- 397-integer-replacement/397-integer-replacement.py
+++ 397-integer-replacement/397-integer-replacement.py
@@ -1,16 +1,22 @@
 class Solution:
     def integerReplacement(self, n: int) -> int:
         # count = 0
-        
+        memo = {}
         def dfs(n):
             if n == 1:
                 return 0
-
+            
+            if n in memo:
+                return memo[n]
+            
+            ans = 0
             if n % 2 == 0:
-                return dfs(n//2) + 1
+                ans = dfs(n >> 1) + 1
             else:
-                adds = dfs(n+1)
-                minus = dfs(n-1)
-                return min(adds, minus) + 1
-        
+                adds = dfs(n + 1)
+                minus = dfs(n - 1)
+                ans = min(adds, minus) + 1
+            memo[n] = ans
+            return ans
+
         return dfs(n)
