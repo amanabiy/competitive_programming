a263915bc083c009fdabe0f841a6b19ce3cc797d
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-07-28 19:48:29 +0300

    Time: 91 ms (12.68%), Space: 14.3 MB (99.36%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,37 +1,28 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        """
-        find the column,find the row
-        """
-#         def binarySearch(find, arr):
-#             mid = len(arr) // 2
-#             left = 0
-#             right = len(arr) - 1
-    
-#             while left <= right:
-#                 mid = (left + right) // 2
-#                 if arr[mid] 
+        ans = False
         
-        # find the column
-        vindex = 0
-        
-        while vindex < len(matrix) and matrix[vindex][0] <= target:
-            if matrix[vindex][0] == target:
-                return True
-            vindex += 1
-        
-        vindex -= 1
-        left = 0
-        right = len(matrix[vindex]) - 1
-        while left <= right:
-            mid = left + (right - left) // 2
-            # print(left, right, mid)
-            if matrix[vindex][mid] == target:
-                return True
-            elif matrix[vindex][mid] < target:
-                left = mid + 1
-            elif matrix[vindex][mid] > target:
-                right = mid - 1
+        def binarySearch(nums, target):
+            left = 0
+            right = len(nums) - 1
+            
+            while left <= right:
+                mid = left + (right - left) // 2
                 
-        return False
+                if nums[mid] == target:
+                    return True
+                
+                elif nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            
+            return False
         
+        
+        for i in range(len(matrix) - 1, -1 , -1):
+            if matrix[i][0] <= target:
+                ans = ans or binarySearch(matrix[i], target)
+        
+        return ans
+            
