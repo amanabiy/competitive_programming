0e7450944284a58c80b171a37e5d62baa51d418d
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-08-01 17:20:36 +0300

    Time: 42 ms (71.26%), Space: 13.9 MB (73.91%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,15 +1,14 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        board = [[0 for i in range(n)] for j in range(m)]
-        board[0][0] = 1
+        grid = [ [0 for i in range(n)] for _ in range(m) ]
+        grid[0][0] = 1
         isValid = lambda x, y: 0 <= x < m and 0 <= y < n
+        
         for i in range(m):
             for j in range(n):
-                left = j - 1
-                up = i - 1
-                if isValid(i, left):
-                    board[i][j] += board[i][left]
-                if isValid(up, j):
-                    board[i][j] += board[up][j]
-        print(board[-1][-1])
-        return board[-1][-1]
+                if isValid(i-1, j):
+                    grid[i][j] += grid[i - 1][j]
+                if isValid(i, j - 1):
+                    grid[i][j] += grid[i][j - 1]
+                # grid[i][j] %= (2 * (10 ** 9)) 
+        return grid[-1][-1] 
