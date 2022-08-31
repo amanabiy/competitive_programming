58c68ddf55112f8ad1d314f76a1e373d7630ef7c
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-08-31 17:42:42 +0300

    Time: 1588 ms (5.01%), Space: 19.2 MB (18.47%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
@@ -1,26 +1,43 @@
 class Solution:
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        n, m = len(matrix), len(matrix[0])
-        directions = [(1,0),(0,1),(-1,0),(0,-1)]
-        isValid = lambda x,y: 0 <= x < n and 0<= y < m
-        visited = set()
-        memo = {}
-        answer = 0
+        ans = 0
+        indegree = [ [ 0 for i in range(len(matrix[0])) ] for _ in range(len(matrix))]
+        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+        isValid = lambda r, c: 0 <= r < len(matrix) and 0 <= c < len(matrix[r])
+        maxLength = [ [ 1 for i in range(len(matrix[0])) ] for _ in range(len(matrix))]
+        graph = defaultdict(list)
+        queue = deque()
         
-        def dfs(i, j):
-            if (i, j) in memo:
-                return memo[(i, j)]
-            visited.add((i, j))
-            ans = [0]
-            for x, y in directions:
-                r, c = i + x, y + j
-                if isValid(r, c) and matrix[i][j] < matrix[r][c]:
-                    ans.append(dfs(r, c))
-            memo[(i,j)] = max(ans) + 1
-            return memo[(i, j)]
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                for x, y in directions:
+                    newR, newC = x + i, y + j
+                    if isValid(newR, newC) and matrix[newR][newC] > matrix[i][j]:
+                        graph[(i, j)].append((newR, newC))
+                        indegree[newR][newC] += 1
         
-        for i in range(n):
-            for j in range(m):
-                answer = max(answer, dfs(i, j))
-        return answer
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if indegree[i][j] == 0:
+                    queue.append((i, j))
+
+        while queue:
+            row, col = queue.popleft()
+            
+            for newR, newC in graph[(row, col)]:
+                indegree[newR][newC] -= 1                    
+                if matrix[newR][newC] > matrix[row][col]:
+                    maxLength[newR][newC] = max(maxLength[newR][newC], 1 + maxLength[row][col])
+
+                if indegree[newR][newC] == 0:
+                    queue.append([newR, newC])
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                ans = max(ans, maxLength[i][j])
+
+        return ans
+        
+                    
+            
             
