3e535e5515a223918a7982e65e0aac51a7745bb8
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-09-07 21:36:55 +0300

    Time: 103 ms (23.26%), Space: 16.3 MB (95.12%) - LeetHub

--- 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.py
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.py
@@ -6,27 +6,21 @@
 #         self.right = right
 class Solution:
     def tree2str(self, root: Optional[TreeNode]) -> str:
-        ans = deque()
         def dfs(node):
             if not node:
-                return deque()
+                return ""
+            
+            ans = str(node.val)
             left = dfs(node.left)
             right = dfs(node.right)
-            
-            
+
             if right:
-                left.appendleft('(')
-                left.append(')')
-                right.appendleft('(')
-                right.append(')')
-                return deque(deque([str(node.val)]) + left + right)
+                ans = ans + "(" + left + ")" +  "(" + right + ")"
             elif left:
-                left.appendleft('(')
-                left.append(')')
-                return deque(deque([str(node.val)]) + left)
-            else:
-                return deque(deque([str(node.val)]))
+                ans = ans + "(" + left + ")"
+            
+            return ans
+            
 
-        ans = dfs(root)
 
-        return ''.join(ans)
+        return dfs(root)
