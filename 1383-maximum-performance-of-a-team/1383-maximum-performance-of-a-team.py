acd971cb53c7edc431ab022b491d6cc1e4079654
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-09-12 00:15:46 +0300

    Time: 1064 ms (6.07%), Space: 29.7 MB (84.21%) - LeetHub

---
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxPerformance(self, n: int, speed: List[int], efficiency: List[int], k: int) -> int:
+        """
+        n - engineers
+        n - speed and effiecency
+        
+        choose k engeeners with maximum
+        preformance = sum(speed) * min(effiecency)
+
+        """
+        cur_sum, h = 0, []
+        ans = -float('inf')
+        
+        for i, j in sorted(zip(efficiency, speed),reverse=True):
+            while len(h) > k-1:
+                cur_sum -= heappop(h)
+            heappush(h, j)
+            cur_sum += j
+            ans = max(ans, cur_sum * i)
+            
+        return ans % (10**9+7)
