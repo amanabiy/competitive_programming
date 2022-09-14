fc0cbb4bba3ae7da721b2d8b00125607d02d579f
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-09-14 08:53:24 +0300

    Time: 1404 ms (50.47%), Space: 86.1 MB (22.74%) - LeetHub

---
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
+        
+        def dfs(node, values):
+            if not node: return 0
+            values[node.val] += 1
+            if not node.left and not node.right:
+                count = 0
+                ret = 1
+                print(values)
+                for key in values:
+                    if values[key] % 2 == 1:
+                        count += 1
+                    if count > 1:
+            
+                        ret = 0
+                values[node.val] -= 1
+                return ret
+
+            left = dfs(node.left, values)
+            right = dfs(node.right, values)
+            values[node.val] -= 1
+            
+            return left + right
+        
+        return dfs(root, defaultdict(int))
+            
+            
+            
+        
