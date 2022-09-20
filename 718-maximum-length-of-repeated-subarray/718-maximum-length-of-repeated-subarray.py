11693d17c2e7deab3238223b673e5119775dd774
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-09-20 11:13:03 +0300

    Time: 5455 ms (56.74%), Space: 40 MB (29.75%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
@@ -7,30 +7,14 @@ class Solution:
           1
           4
         """
-        dp = [[0 for _ in nums1] for _ in nums2]
-        ans = 0  
+        dp = [[0 for _ in range(len(nums1) + 1)] for _ in range(len(nums2) + 1)]
+        ans = 0 
         
-        # the first row to 1 if matched
-        for i in range(len(nums1)):
-            if nums1[i] == nums2[0]:
-                dp[0][i] = 1
-                ans = 1
-            # if i > 0:
-            #     dp[0][i] = max(dp[0][i], dp[0][i - 1])
-        
-        # the first col to 1 if matched
         for i in range(len(nums2)):
-            if nums2[i] == nums1[0]:
-                dp[i][0] = 1
-                ans = 1
-
-        
-        for i in range(1, len(nums2)):
-            for j in range(1, len(nums1)):
+            for j in range(len(nums1)):
                 if nums2[i] == nums1[j]:
-                    dp[i][j] = dp[i - 1][j - 1] + 1
-
-                if ans < dp[i][j]:
-                    ans = dp[i][j]
+                    dp[i + 1][j + 1] = dp[i][j] + 1
+                    if ans < dp[i + 1][j + 1]:
+                        ans = dp[i + 1][j + 1]
 
         return ans
