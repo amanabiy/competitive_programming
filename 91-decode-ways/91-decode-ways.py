71330eaf1df644e8f3a566914e4b45228858d81d
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-09-23 17:48:51 +0300

    Time: 57 ms (43.22%), Space: 14.1 MB (19.52%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,24 +1,26 @@
 class Solution:
+
     def numDecodings(self, s: str) -> int:
-        """
-        1 11 0 6
-        1 11 0 6
-        """
-        word = list(s)
-        memo = {}
+        letters = {}
+        start = ord('A')
+        memo = [float('inf')] * len(s)
+        for i in range(start, ord('Z') + 1):
+            letters[chr(i)] = i - start + 1
         
-        def dfs(i):            
-            if i == len(word):
+        def findWays(i):
+            if i >= len(s):
                 return 1
-            if i in memo:
+            if memo[i] != float('inf'):
                 return memo[i]
-            ans = 0
-            if word[i] != "0":
-                ans += dfs(i+1)
-                if i < len(word) - 1 and word[i] + word[i+1] <= "26":
-                    ans += dfs(i+2)
             
-            memo[i] = ans
-            return ans
+            one, two = 0, 0
+            
+            if s[i] != '0':
+                one = findWays(i+1)
+                if i < len(s) - 1 and s[i:i+2] <= '26':
+                    two = findWays(i+2)
+
+            memo[i] = two + one
+            return memo[i]
         
-        return dfs(0)
+        return findWays(0)
