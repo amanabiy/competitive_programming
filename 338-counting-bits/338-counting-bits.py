c76d34e674a3bf2c561c41ab9f7cefd74e4d6cf5
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-09-25 11:22:30 +0300

    Time: 146 ms (60.12%), Space: 20.9 MB (30.52%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -5,13 +5,10 @@ class Solution:
         1
         
         """
-        ans = []
+        ans = [0]
 
-        for i in range(n + 1):
-            count = 0
-            for j in range(32):
-                if i & (1 << j):
-                    count += 1
+        for i in range(1, n + 1):
+            count = ans[i & (i - 1)] + 1
             ans.append(count)
         
         return ans
