aabc64a9819cd29d4c46014c46ce094f7755f147
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-10-07 14:45:15 +0300

    Time: 328 ms (47.90%), Space: 20.5 MB (38.98%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
@@ -1,28 +1,17 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        """
         
-        def binarySearch(arr, target):
-            left = 0
-            right = len(arr) - 1
-            while left <= right:
-                mid = left + ((right - left) // 2)
-                
-                if arr[mid] == target:
-                    return True
-                elif arr[mid] < target:
-                    left = mid + 1
-                elif arr[mid] > target:
-                    right = mid - 1
-            
-            return False
+        """
         
-    
-        i = 0
-        while i < len(matrix):
-            if matrix[i][0] > target:
-                break
-            if binarySearch(matrix[i], target):
+        row, col = 0, len(matrix[0]) - 1
+        
+        while row < len(matrix) and col >= 0:
+            if matrix[row][col] == target:
                 return True
-            i += 1
+            elif matrix[row][col] < target:
+                row += 1
+            else:
+                col -= 1
         
         return False
