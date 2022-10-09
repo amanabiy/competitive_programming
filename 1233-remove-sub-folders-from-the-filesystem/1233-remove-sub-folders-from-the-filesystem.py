d9a456eac96f7e6c2d3e2586b975b7d6bc1b5985
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-10-09 12:16:55 +0300

    Time: 577 ms (44.01%), Space: 42.6 MB (16.17%) - LeetHub

---
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.py
@@ -0,0 +1,43 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isWord = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+           
+    def insert(self, word):
+        current = self.root
+        
+        word = word.split('/')[1:]
+        for char in word:
+            if char not in current.children:
+                current.children[char] = TrieNode()
+            current = current.children[char]
+        current.isWord = True        
+
+    def allRootFolders(self):
+        ans = []
+        def dfs(node, strs):
+            # if not node.children:
+            #     return
+            if node.isWord:
+                ans.append("/"+"/".join(strs))
+            else:
+                for key in node.children:
+                    dfs(node.children[key], strs+[key])
+        dfs(self.root, [])
+        return ans
+
+    
+class Solution:
+    def removeSubfolders(self, folder: List[str]) -> List[str]:
+        folders = Trie()
+        ans = []
+        
+        for f in folder:
+            if f != '' and f != '/':
+                folders.insert(f)
+        
+        return folders.allRootFolders()
