ac350884c02d87feb9f3ca6c0812b2963720c384
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-10-13 04:45:38 +0300

    Time: 1490 ms (5.03%), Space: 14.5 MB (49.53%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -1,6 +1,6 @@
 class Solution:
     def isBipartite(self, AList: List[List[int]]) -> bool:
-        visited = set()
+        color = {}
 
         graph = defaultdict(list)
         
@@ -10,32 +10,26 @@ class Solution:
                 graph[edge].append(i)
         
         def bfs(node):
-            visited.add(node)
             queue = deque([node])
-            setA = set([node])
-            setB = set()
+            color = {node: True}
+            
             while queue:
                 n = queue.popleft()
                 # check 
                 for child in graph[n]:
-                    if child in visited:
-                        if n in setA and child in setA:
-                            return False
-                        if n in setB and child in setB:
+                    if child in color:
+                        if color[child] == color[n]:
                             return False
                     else:
-                        if n in setA:
-                            setB.add(child)
-                        else:
-                            setA.add(child)
-                        visited.add(child)
+                        childColor = not color[n]
+                        color[child] = childColor
                         queue.append(child)
             
             return True
 
         
         for i in range(len(AList)):
-            if i not in visited:
+            if i not in color:
                 if not bfs(i):
                     return False
         
