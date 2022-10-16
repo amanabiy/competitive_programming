0945c329b66101a11fe62cecfc130e518cd575cf
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-10-16 20:15:25 +0300

    Time: 4857 ms (23.43%), Space: 158.1 MB (30.23%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.py
@@ -9,34 +9,27 @@ class Solution:
         """
         n = len(parent)
         visited = set()
-        queue = deque()
         graph = defaultdict(list)
-        ans = [ 1 for _ in range(n) ]
-        indegree = [ 0 for _ in range(n) ]
-        maxAns = 1
+        self.maxAns = 1
 
         for i in range(1, n):
             graph[parent[i]].append(i)
-            graph[i].append(parent[i])
-            indegree[parent[i]] += 1
-        
-        for i in range(n):
-            if indegree[i] == 0:
-                queue.append(i)
-                visited.add(i)
-        # print("queue", queue, indegree)
-        while queue:
-            n = queue.popleft()
 
-            for neigh in graph[n]:
-                if neigh not in visited :
-                    indegree[neigh] -= 1
-                    if indegree[neigh] == 0:
-                        # print(ans[neigh])
-                        queue.append(neigh)
-                        visited.add(neigh)
-                    if s[neigh] != s[n]:
-                        maxAns = max(ans[neigh] + ans[n], maxAns)
-                        ans[neigh] = max(ans[neigh], ans[n] + 1)
-        # print(ans, indegree, maxAns)
-        return maxAns
+        def dfs(i):
+            if i > len(parent):
+                return 0
+        
+            candi = [0,0]
+            for child in graph[i]:
+                score = dfs(child)
+                if s[i] != s[child]:
+                    candi.append(score)
+    
+            candi.sort()
+            self.maxAns = max(candi[-1] + candi[-2]+ 1, self.maxAns)
+            return candi[-1] + 1
+        
+        dfs(0)
+
+        return self.maxAns
+            
