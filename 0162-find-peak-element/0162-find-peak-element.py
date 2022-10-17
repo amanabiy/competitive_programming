ebcf10b88be1914a92fd6d1369e0ff1a614952cc
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-10-17 14:38:30 +0300

    Time: 46 ms (95.94%), Space: 13.9 MB (98.45%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -5,23 +5,18 @@ class Solution:
         
         left = 0
         right = len(nums)
-        # nums = [float('-inf')] + nums + [float('-inf')]
+
         while left < right:
             mid = left + (right - left) // 2
-            
-            if (mid - 1 < 0 and nums[mid + 1] < nums[mid]) or (len(nums) == mid + 1 and nums[mid - 1] < nums[mid]):
-                return mid
-            # print(mid, left, right)
-            if nums[mid - 1] < nums[mid] and nums[mid] > nums[mid + 1]:
-                return mid
             
             if nums[mid] > nums[mid - 1]:
                 left = mid + 1
             else:
                 right = mid
+            # print(left, right, mid)
     
-        print("not reachable")
-        return left
+        # print("not reachable")
+        return left - 1 if left > 0 else 0
                                                               
             
             
