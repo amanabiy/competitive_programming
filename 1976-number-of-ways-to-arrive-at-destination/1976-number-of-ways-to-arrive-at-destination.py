a4d6ba888dbd82b8917b725c6c8a40f02fb3bca8
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-10-18 20:38:42 +0300

    Time: 638 ms (54.97%), Space: 20.6 MB (53.25%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.py
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.py
@@ -1,12 +1,10 @@
 class Solution:
     def countPaths(self, n: int, roads: List[List[int]]) -> int:
-        count_path = defaultdict(int)
         graph = defaultdict(list)
-        shortestTime = [inf] * n 
         dp = [inf] * n 
+        shortestTime = [inf] * n
         ans = 0
         heap = []
-        timeee = 0
 
         for i, j, k in roads:
             graph[i].append((j, k))
@@ -22,15 +20,15 @@ class Solution:
             if node == n - 1:
                 return dp[-1]
 
-            if shortestTime[node] < Times:
-                continue
+            # if shortestTime[node] < Times:
+            #     continue
         
-            shortestTime[node] = Times
+            # shortestTime[node] = Times
 
             for child, time in graph[node]:
                 if shortestTime[child] > time + Times:
-                    shortestTime[child] = time + Times
                     heapq.heappush(heap, (Times + time, child))
+                    shortestTime[child] = time + Times
                     dp[child] = dp[node]
                 elif shortestTime[child] == time + Times:
                     dp[child] = (dp[child] + dp[node]) % (10 ** 9 + 7) 
