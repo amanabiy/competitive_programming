97ec68371f5ee675c82dc40ab3452fa865b07ff2
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-10-23 13:24:37 +0300

    Time: 7967 ms (6.92%), Space: 122.2 MB (16.53%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -4,18 +4,34 @@ class Solution:
             1, 5, 11, 5
         
         """
-        l, s = len(nums), sum(nums)
-        @cache  # this is important to avoid unnecessary recursion
-        def dfs(curr: int, idx: int) -> bool:
-            """
-            Select elements and check if nums can be partitioned.
-            :param curr: The current sum of the elements in the subset.
-            :param idx: The index of the current element in nums.
-            :return: True if nums can be partitioned, False otherwise.
-            """
-            if idx == l:  # we have reached the end of the array
-                return curr == s>>1
-            elif curr+nums[idx] == s>>1 or (curr+nums[idx] < s>>1 and dfs(curr+nums[idx], idx+1)):  # or, target sum will be reached by selecting this element
+        allSum = sum(nums)
+        nums.sort(reverse=True)
+        def dfs(i, target, memo):
+            
+            if target == 0:
                 return True
-            return dfs(curr, idx+1)  # else, don't select this element, continue
-        return False if s&1 else dfs(0, 0)
+            
+            if i >= len(nums):
+                return False
+
+            if (i, target) in memo:
+                return memo[(i, target)]
+            
+            # take this element
+            res = dfs(i + 1, target - nums[i], memo)
+            
+            if res:
+                memo[(i, target)] = True
+                return True
+
+            # not take this element
+            not_take = dfs(i + 1, target, memo)
+            memo[(i, target)] = not_take
+
+            return memo[(i, target)]
+
+        
+        if allSum % 2 == 1:
+            return False
+        # print(allSum)
+        return dfs(0, allSum // 2, {0: True})
