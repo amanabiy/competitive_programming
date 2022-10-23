f904ec97d08ed533f7ea346f0db79fe9a083a35c
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-10-23 13:20:11 +0300

    Time: 2149 ms (54.80%), Space: 150 MB (11.27%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -4,19 +4,18 @@ class Solution:
             1, 5, 11, 5
         
         """
-        totalSum = sum(nums)
-        
-        if totalSum % 2 == 1:
-            return False
-        seen = set([0])        
-        half =  totalSum // 2
-
-        for num in nums:
-            collect = []
-            for x in seen:
-                if x + num == half:
-                    return True
-                collect.append(x + num)
-            seen = seen | set(collect)
-
-        return False
+        l, s = len(nums), sum(nums)
+        @cache  # this is important to avoid unnecessary recursion
+        def dfs(curr: int, idx: int) -> bool:
+            """
+            Select elements and check if nums can be partitioned.
+            :param curr: The current sum of the elements in the subset.
+            :param idx: The index of the current element in nums.
+            :return: True if nums can be partitioned, False otherwise.
+            """
+            if idx == l:  # we have reached the end of the array
+                return curr == s>>1
+            elif curr+nums[idx] == s>>1 or (curr+nums[idx] < s>>1 and dfs(curr+nums[idx], idx+1)):  # or, target sum will be reached by selecting this element
+                return True
+            return dfs(curr, idx+1)  # else, don't select this element, continue
+        return False if s&1 else dfs(0, 0)
