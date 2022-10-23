b6f1af0c9a09104374038abcf94c2358cba731e3
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-10-23 10:43:32 +0300

    Time: 136 ms (11.73%), Space: 14 MB (73.43%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -2,24 +2,27 @@ class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         if len(s1) + len(s2) != len(s3):
             return False
-        memo = {}
+    
+        s1Len = len(s1)
+        s2Len = len(s2)
+        dp = [ [ False for _ in range(s2Len + 1) ] for _ in range(s1Len + 1)]
         
-        def dfs(i, j, k):
-            if k == len(s3):
-                return True
-            
-            if (i, j, k) in memo:
-                return memo[(i, j, k)]
+        dp[0][0] = True
 
-            ans = False
-
-            # check if the two letters are same
-            if i < len(s1) and s1[i] == s3[k]:
-                ans = ans or dfs(i + 1, j, k + 1)
-            if j < len(s2) and s2[j] == s3[k]:
-                ans = ans or dfs(i, j + 1, k + 1)
-
-            memo[(i, j, k)] = ans
-            return ans
+        for row in range(1, s1Len + 1):
+            dp[row][0] = s1[row - 1] == s3[row - 1] and dp[row - 1][0]
         
-        return dfs(0, 0, 0)
+        
+        for col in range(1, s2Len + 1):
+            dp[0][col] = s2[col - 1] == s3[col - 1] and dp[0][col - 1]
+        
+        
+        for row in range(1, len(dp)):
+            for col in range(1, len(dp[row])):
+                if s1[row - 1] == s3[row + col - 1]:
+                    dp[row][col] = dp[row - 1][col]
+                if s2[col - 1] == s3[row + col - 1]:
+                    dp[row][col] = dp[row][col] or dp[row][col - 1]
+        
+        return dp[-1][-1]
+        
