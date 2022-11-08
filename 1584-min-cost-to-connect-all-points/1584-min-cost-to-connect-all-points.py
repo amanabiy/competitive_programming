8a5c651b028bca33a83a76f0829eb54b82b6d76a
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-11-08 13:36:42 +0300

    Time: 3931 ms (55.13%), Space: 87 MB (63.45%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,31 +1,18 @@
 class Solution:
-    def find(self, node, parent):
-        if parent[node] == -1:
-            return node
-        parent[node] = self.find(parent[node], parent)
-        return parent[node]
-
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        allConnection = []
         totalCost = 0
-        parents = defaultdict(lambda: -1)
-        rank = defaultdict(lambda: 1)
+        visited = set()
+        frontier = [[0, 0]]
+        
+        while len(visited) < len(points):
+            distance, node = heapq.heappop(frontier)
+            if node in visited:
+                continue
+            visited.add(node)
+            totalCost += distance
+            for i in range(len(points)):
+                if i not in visited:
+                    d = abs(points[node][0] - points[i][0]) + abs(points[node][1] - points[i][1])
+                    heapq.heappush(frontier, [d, i])
 
-        # consider connecting every point to eachother
-        for i, (x1, y1) in enumerate(points):
-            for j in range(i + 1, len(points)):
-                # calculate the distance between each point
-                distance = abs(x1 - points[j][0]) + abs(y1 - points[j][1]) 
-                allConnection.append([distance, (x1, y1), tuple(points[j])])
-        
-        allConnection.sort()
-        
-        for cost, point1, point2 in allConnection:
-            par1 = self.find(point1, parents)
-            par2 = self.find(point2, parents)
-            
-            if par1 != par2:
-                totalCost += cost
-                parents[par1] = par2
-        
         return totalCost
