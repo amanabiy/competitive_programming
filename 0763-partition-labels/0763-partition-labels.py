7738e28b7a19f9a22f7bb13c868770efa0f552f5
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-11-24 16:08:34 +0300

    Time: 100 ms (18.93%), Space: 13.9 MB (69.23%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.py
@@ -0,0 +1,27 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        """
+        a  b  a  b   c   bacadefegdehijhklij
+        a  ab ab ab  abc
+        abc
+        defg
+        hijkl
+        
+        
+        """
+        ans = []
+        lastSeen = {}
+        for i in range(len(s)):
+            lastSeen[s[i]] = i
+        
+        left = 0
+        right = 0
+        while right < len(s) and left < len(s):
+            end = lastSeen[s[left]]
+            while right <= end:
+                end = max(lastSeen[s[right]], end)
+                right += 1
+            ans.append(right - left)
+            left = right
+
+        return ans
