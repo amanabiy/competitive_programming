f47ca67dcfc83512b69cc20401feface1a406b34
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-11-30 14:12:30 +0300

    Time: 5405 ms (42.48%), Space: 76.2 MB (14.52%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
@@ -1,23 +1,26 @@
 class Solution:
     def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
-        heap = []
-        gotSecret = {0: -1}
-        meeting = defaultdict(list)
-        meeting[0].append((firstPerson, 0))
+        minHeap = []
+        graph = defaultdict(list)
+        knowSecret = {0: 0, firstPerson: 0}
 
-        for x, y, time in meetings:
-            meeting[x].append((y, time))
-            meeting[y].append((x, time))
-        
-        heap.append((-1,0))
-        # print(meeting)
-        while heap:
-            parentTime, node = heapq.heappop(heap)
-            
-            for person, time in meeting[node]:
-                if time >= parentTime and ((person not in gotSecret) or (person in gotSecret and time < gotSecret[person])):
-                    gotSecret[person] = time
-                    heapq.heappush(heap, (time, person))
-                    
-            # print(heap, gotSecret)
-        return gotSecret.keys()
+
+        for p1, p2, t1 in meetings:
+            graph[p1].append([t1, p2])
+            graph[p2].append([t1, p1])
+
+
+        minHeap = [[0, firstPerson], [0, 0]]
+
+
+        while minHeap:
+            time, person = heapq.heappop(minHeap)
+            if time > knowSecret[person]:
+                continue
+            for t, ps in graph[person]:
+                if t >= time and (ps not in knowSecret or (ps in knowSecret and t < knowSecret[ps])):
+                    knowSecret[ps] = t
+                    heapq.heappush(minHeap, [t, ps])
+
+
+        return list(knowSecret.keys())
