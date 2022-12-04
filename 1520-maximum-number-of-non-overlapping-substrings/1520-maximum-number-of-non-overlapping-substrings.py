16d98e80b361abec00a5ad585e369bb176cbc3fb
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-12-04 22:01:40 +0300

    Time: 581 ms (66.67%), Space: 15.3 MB (87.50%) - LeetHub

---
+++ 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.py
@@ -0,0 +1,44 @@
+class Solution:
+    def maxNumOfSubstrings(self, s: str) -> List[str]:
+        def expandEnd(s, startIndex, last, visited):
+            end = last[s[startIndex]]
+            for i in range(startIndex, len(s)):
+                if i > end:
+                    break
+                if s[i] in visited:
+                    return -1
+                end = max(last[s[i]], end)
+            
+            return end
+
+
+        last = {}
+        seen = set()
+        ans = []
+
+        # get the last position of the letters
+        for i, l in enumerate(s):
+            last[l] = i
+
+        end = float('-inf')
+
+        # Iterate and try to expand
+        for i in range(len(s)):
+            if s[i] not in seen:
+                lastInd = expandEnd(s, i, last, seen)
+                # consider whether to add it to ans or just replace the previous one
+
+                if lastInd != -1:
+                    if end < i:
+                        ans.append(s[i:lastInd + 1])
+                    elif lastInd <= end:
+                        ans[-1] = s[i:lastInd + 1]
+                    end = lastInd
+
+                seen.add(s[i])
+
+        return ans
+        
+        
+        
+# print(getValidSubstring("adefaddaccc"))
