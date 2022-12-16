78ca7d22e9075d4490e4948250d9623dfe768232
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-12-16 22:33:09 +0300

    Time: 2520 ms (80.60%), Space: 57.1 MB (85.07%) - LeetHub

--- 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.py
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.py
@@ -1,4 +1,17 @@
 class Solution:
+    def right(self, arr, val):
+        lo = 0
+        hi = len(arr) - 1
+        
+        while lo <= hi:
+            mid = lo + (hi - lo) // 2
+            if arr[mid] <= val:
+                lo = mid + 1
+            else:
+                hi = mid - 1
+        
+        return lo
+                
     def maxTwoEvents(self, events: List[List[int]]) -> int:
         prefixSum = [0]
         ans = float('-inf')
@@ -10,7 +23,7 @@ class Solution:
         
         prefixSum = prefixSum[::-1]
         for i in range(len(events)):
-            index = bisect.bisect_right(startTime, events[i][1])
+            index = self.right(startTime, events[i][1])
             ans = max(ans, prefixSum[index] + events[i][2])
         
         return ans
