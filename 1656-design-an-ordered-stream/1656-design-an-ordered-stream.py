8f4b1205cd02417d9ae6e3378ce3d15cbb775a12
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-12-19 12:00:09 +0300

    Time: 329 ms (67.50%), Space: 14.7 MB (48.05%) - LeetHub

--- 1656-design-an-ordered-stream/1656-design-an-ordered-stream.py
+++ 1656-design-an-ordered-stream/1656-design-an-ordered-stream.py
@@ -2,14 +2,14 @@ class OrderedStream:
 
     def __init__(self, n: int):
         self.ptr = 0
-        self.arr = [""] * n
+        self.arr = {}
 
     def insert(self, idKey: int, value: str) -> List[str]:
         chunks = []
         idKey -= 1
         self.arr[idKey] = value
         if self.ptr == idKey:
-            while self.ptr < len(self.arr) and self.arr[self.ptr]:
+            while self.ptr in self.arr:
                 chunks.append(self.arr[self.ptr])
                 self.ptr += 1
         
