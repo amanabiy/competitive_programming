2bbe1e23a08d8687ac59c20722bfb8891c711108
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-12-22 10:33:51 +0300

    Time: 427 ms (85.38%), Space: 30.2 MB (30.69%) - LeetHub

--- 1583-count-unhappy-friends/1583-count-unhappy-friends.py
+++ 1583-count-unhappy-friends/1583-count-unhappy-friends.py
@@ -10,30 +10,19 @@ class Solution:
           0: {1,3}
         }
         """
-        def findBetterFriends(pref, pairedWith):
-            better = set()
-            for i in pref:
-                if i == pairedWith:
-                    break
-                better.add(i)
-            return better
-        
-        def findUnHappy(betterOpt, person, unhappy):
-            for better in betterOpt:
+        def findUnHappy(person, unhappy, betterOptions):
+            for better in betterOptions[person]:
                 if person in betterOptions[better]:
                     unhappy.add(better)
                     unhappy.add(person)
-        
+
         unhappy = set()
         betterOptions = defaultdict(set)
 
         for person1, person2 in pairs:
-            betterOptions[person1] = findBetterFriends(preferences[person1], person2)
-            betterOptions[person2] = findBetterFriends(preferences[person2], person1)
-            
-            findUnHappy(betterOptions[person1], person1, unhappy)
-            findUnHappy(betterOptions[person2], person2, unhappy)
-        
+            betterOptions[person1] = set(preferences[person1][:preferences[person1].index(person2)])
+            betterOptions[person2] = set(preferences[person2][:preferences[person2].index(person1)])
+            findUnHappy(person1, unhappy, betterOptions)
+            findUnHappy(person2, unhappy, betterOptions)
+
         return len(unhappy)
-            
-            
