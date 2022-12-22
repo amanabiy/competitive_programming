da9b15cf8694d6776d21d63241ebec4cf4096f90
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-12-22 11:17:57 +0300

    Time: 34 ms (86.61%), Space: 13.9 MB (82.60%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.py
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -1,25 +1,27 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
         memo = [ set() for _ in range(len(s))]
+        n = len(s)
         
-        def dfs(i, s, wordDict):
+        def dfs(i, s, wordDict, n):
             if i >= len(s):
                 return {''}
 
             if memo[i]:
                 return memo[i]
-        
-            for j in range(i+1, i+11):
+
+            size = min(i+11, n+1)
+            for j in range(i+1, size):
                 word = s[i:j]
                 if word in wordDict:
-                    got = dfs(j, s, wordDict)
+                    got = dfs(j, s, wordDict, n)
                     for sf in got:
                         split = " " if sf else ""
                         memo[i].add(word + split + sf)
             
             return memo[i]
         
-        return dfs(0, s, set(wordDict))
+        return dfs(0, s, set(wordDict), n)
         
 
             
