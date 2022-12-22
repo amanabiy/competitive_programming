611014ee9e3214122e12913be9f1af87e0eceda7
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-12-22 22:21:05 +0300

    Time: 1058 ms (85.00%), Space: 67.2 MB (13.33%) - LeetHub

---
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.py
@@ -0,0 +1,39 @@
+class Solution:
+    def sumOfDistancesInTree(self, n: int, edges: List[List[int]]) -> List[int]:
+        """
+        """
+        memo = {}
+        answer = [0] * n
+        graph = defaultdict(list)
+        
+        for x, y in edges:
+            graph[x].append(y)
+            graph[y].append(x)
+
+        def dfs(node, parent):
+            ans = [0, 1]
+            for child in graph[node]:
+                if child != parent:
+                    t = dfs(child, node)
+                    ans[0] += t[0]
+                    ans[1] += t[1]
+            memo[node] = ans[::]
+            ans[0] += ans[1]
+            return ans
+
+        def getTotalAns(node, parent, answer):
+            if parent != -1:
+                thisChilds = memo[node][0] + memo[node][1]
+                answer[node] = answer[parent] - thisChilds + (n - memo[node][1]) + memo[node][0]
+                # calculate
+                
+            for child in graph[node]:
+                if child != parent:
+                    getTotalAns(child, node, answer)
+
+        dfs(0, -1)
+        answer[0] = memo[0][0]
+        getTotalAns(0, -1, answer)
+
+        return answer
+        
