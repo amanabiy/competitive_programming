78937356c0f3215cdb16399ec5078299852e2a3e
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-12-23 14:36:40 +0300

    Time: 775 ms (95.47%), Space: 75.9 MB (38.54%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,59 +1,26 @@
-class Node:
-    def __init__(self, key=None, val=None, nextP=None, prev=None):
-        self.key = key
-        self.val = val
-        self.next = nextP
-        self.prev = prev
-
-class DoublyLinkedList:
-    def __init__(self):
-        self.head = Node(-1, -1)
-        self.tail = Node(-1, -1)
-        self.head.next = self.tail
-        self.tail.prev = self.head
-    
-    def add(self, key, val):
-        lastNode = self.tail.prev
-        newNode = Node(key, val)
-        newNode.next = lastNode.next
-        lastNode.next = newNode
-        newNode.prev = lastNode
-        self.tail.prev = newNode
-        return newNode
-        
-    def delete(self, node):
-        prev = node.prev
-        prev.next = node.next
-        node.next.prev = prev
-        del node
-    
-    def getFrist(self):
-        if self.head.next != self.tail:
-            return self.head.next
-
+from collections import OrderedDict
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.nodes = DoublyLinkedList()
-        self.mapNodes = {}
+        self.keyValue = OrderedDict()
 
     def get(self, key: int) -> int:
-        if key not in self.mapNodes:
+        if key not in self.keyValue:
             return -1
-        node = self.mapNodes[key].val
-        self.nodes.delete(self.mapNodes[key])
-        self.mapNodes[key] = self.nodes.add(key, node)
-        return node
-    
+        self.keyValue.move_to_end(key)
+        return self.keyValue[key]
+
     def put(self, key: int, value: int) -> None:
-        if key in self.mapNodes:
-            self.nodes.delete(self.mapNodes[key])
-        if len(self.mapNodes) == self.capacity and key not in self.mapNodes:
-            self.mapNodes.pop(self.nodes.head.next.key)
-            self.nodes.delete(self.nodes.head.next)
-        self.mapNodes[key] = self.nodes.add(key, value)
-    
+        if key in self.keyValue:
+            self.keyValue[key] = value
+            self.keyValue.move_to_end(key)
+            return
+        if len(self.keyValue) == self.capacity:
+            self.keyValue.popitem(last=False)
+        self.keyValue[key] = value
+
+
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
