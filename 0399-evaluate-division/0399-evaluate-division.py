2d07e3f8fe616933bcef4afc4d7b28618321cec5
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-12-27 12:17:43 +0300

    Time: 31 ms (93.14%), Space: 14.1 MB (15.45%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,28 +1,28 @@
 class Solution:
-    def bfs(self, a, b, graph):
-        if a not in graph:
-            return -1
-
-        queue = deque([(a, 1)])
-        visited = set([(a, 1)])
-        
-        while queue:
-            key, val = queue.popleft()
-            
-            if key == b:
-                return val
-
-            for child in graph[key][0]:
-                if child not in visited:
-                    visited.add(child)
-                    queue.append((child, val * graph[key][1][child]))
-
-        return -1
-
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         graph = defaultdict(lambda: [set(), {}])
         n = len(equations)
         ans = []
+        
+        def bfs(a, b, graph):
+            if a not in graph:
+                return -1
+
+            queue = deque([(a, 1)])
+            visited = set([(a, 1)])
+
+            while queue:
+                key, val = queue.popleft()
+
+                if key == b:
+                    return val
+
+                for child in graph[key][0]:
+                    if child not in visited:
+                        visited.add(child)
+                        queue.append((child, val * graph[key][1][child]))
+
+            return -1
 
         for i in range(n):
             x, y = equations[i]
@@ -32,6 +32,6 @@ class Solution:
             graph[y][1][x] = 1 / values[i]
         
         for froQuery, toQuery in queries:
-            ans.append(self.bfs(froQuery, toQuery, graph))  
+            ans.append(bfs(froQuery, toQuery, graph))  
         
         return ans
