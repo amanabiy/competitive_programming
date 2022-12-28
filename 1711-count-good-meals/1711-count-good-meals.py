4b6da3badc0517a9d0cf024aca96319095820024
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-12-29 00:51:09 +0300

    Time: 814 ms (93.48%), Space: 21.9 MB (51.56%) - LeetHub

---
+++ 1711-count-good-meals/1711-count-good-meals.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countPairs(self, deliciousness: List[int]) -> int:
+        seen = defaultdict(int)
+        mod = 10 ** 9 + 7
+        ans = 0
+        
+        deliciousness.sort()
+        for num in deliciousness:
+            nextNum = 1 << (len(bin(num)) - 2)
+            if nextNum - num in seen:
+                ans = (ans + seen[nextNum - num]) % mod
+            if num != 0 and (num - 1) & (num) == 0:
+                ans = (ans + seen[0]) % mod
+            seen[num] += 1
+
+        return ans
