274d31413e06d82af8d9ce581db833aa1ee4a1b4
Amanuel Abiy <amanuelabiy.as@gmail.com>
2022-12-29 01:01:46 +0300

    Time: 996 ms (87.53%), Space: 21.6 MB (71.39%) - LeetHub

--- 1711-count-good-meals/1711-count-good-meals.py
+++ 1711-count-good-meals/1711-count-good-meals.py
@@ -4,13 +4,20 @@ class Solution:
         mod = 10 ** 9 + 7
         ans = 0
         
+        def numBits(num):
+            count = 0
+            while num:
+                num >>= 1
+                count += 1
+            return count
+        
         deliciousness.sort()
         for num in deliciousness:
-            nextNum = 1 << (len(bin(num)) - 2)
+            nextNum = 1 << (numBits(num))
             if nextNum - num in seen:
                 ans = (ans + seen[nextNum - num]) % mod
             if num != 0 and (num - 1) & (num) == 0:
-                ans = (ans + seen[0]) % mod
+                ans = (ans + seen[0]) % mod            
             seen[num] += 1
 
         return ans
