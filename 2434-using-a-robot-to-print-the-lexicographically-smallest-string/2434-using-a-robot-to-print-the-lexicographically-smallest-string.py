61c63ac7cf6e7196d70e6531e8a2268e12cf12ec
Amanuel Abiy <amanuelabiy.as@gmail.com>
2023-01-01 17:04:40 +0300

    Time: 4196 ms (24.02%), Space: 22.5 MB (5.39%) - LeetHub

--- 2434-using-a-robot-to-print-the-lexicographically-smallest-string/2434-using-a-robot-to-print-the-lexicographically-smallest-string.py
+++ 2434-using-a-robot-to-print-the-lexicographically-smallest-string/2434-using-a-robot-to-print-the-lexicographically-smallest-string.py
@@ -1,17 +1,23 @@
 class Solution:
     def robotWithString(self, s: str) -> str:
         """
-        s -> "cawerbasf"
+        s -> "cawerb a sf"
               aaaaaa a ff -> 26 * n -> 26 charcter -> bit
                      sf
         t -> cwerbs
         paper -> aa
         """
         n = len(s)
-        lessInFuture = Counter(s)
+        lessInFuture = [0] * (n + 1)
+        a = ord('a')
         t = []
         ans = []
-
+        
+        # making the prefix array
+        for i in range(n - 1, -1, -1):
+            letter = ord(s[i]) - a
+            lessInFuture[i] = lessInFuture[i + 1] | 1 << letter
+        
         # add a dummy data to remove all at the end
         s += chr(ord('z') + 1)
 
@@ -19,15 +25,14 @@ class Solution:
         for i in range(n + 1):
             while t and ord(t[-1]) <= ord(s[i]):
                 allowPop = True
-                for num in range(ord('a'), ord(t[-1])):
-                    if lessInFuture[chr(num)] > 0:
+                for num in range(ord(t[-1]) - ord('a')):
+                    if lessInFuture[i] & (1 << num):
                         allowPop = False
                         break
                 if allowPop:
                     ans.append(t.pop())
                 else:
                     break
-            lessInFuture[s[i]] -= 1
             t.append(s[i])
 
         return ''.join(ans)
