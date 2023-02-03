f1e2598b1d8db52452ad006f7316b4247d751429
Amanuel Abiy <amanuelabiy.as@gmail.com>
2023-02-03 19:10:44 +0300

    Time: 108 ms (74.09%), Space: 14.5 MB (86.30%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,15 +1,4 @@
 class Solution:
-    def checkPalindrome(self, s):
-        left = 0
-        right = len(s) - 1
-        
-        while left < right:
-            if s[left] != s[right]:
-                return False
-            left += 1
-            right -= 1
-        return True
-
     def validPalindrome(self, s: str) -> bool:
         left = 0
         right = len(s) - 1
@@ -22,8 +11,8 @@ class Solution:
             right -= 1
         
         if left < right:
-            x = self.checkPalindrome(s[:left] + s[left+1:])
-            y = self.checkPalindrome(s[:right] + s[right+1:])
+            x = s[:left] + s[left+1:] == (s[:left] + s[left+1:])[::-1]
+            y = s[:right] + s[right+1:] == (s[:right] + s[right+1:])[::-1]
             ans = x or y
         
         return ans
