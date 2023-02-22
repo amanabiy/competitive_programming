4f4eb7349ea6704c3ab8812d1b55c3612bf9cf39
Amanuel Abiy <amanuelabiy.as@gmail.com>
2023-02-22 22:32:17 +0300

    Time: 4002 ms (10.72%), Space: 13.9 MB (25.40%) - LeetHub

--- 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.py
+++ 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.py
@@ -10,25 +10,18 @@ class Solution:
         time = 0
         s = list(s)
         swap = True
-        toSwap = deque()
-        
-        for i in range(len(s) - 1):
-            if s[i] == "0" and s[i + 1] == "1":
-                toSwap.append(i)
-
-        while toSwap:
-            nextLevel = deque()
-            for j in range(len(toSwap)):
-                i = toSwap.pop()
-                s[i], s[i + 1] = s[i + 1], s[i]
-                if i + 2 < len(s) and s[i + 2] == "1":
-                    nextLevel.appendleft(i + 1)
-                if i > 0 and s[i - 1] == "0":
-                    nextLevel.appendleft(i - 1)
-    
+        while swap:
+            swap = False
+            i = 0
+            while i < len(s) - 1:
+                if s[i] == "0" and s[i + 1] == "1":
+                    s[i], s[i + 1] = s[i + 1], s[i]
+                    swap = True
+                    i += 1
+                i += 1
             time += 1
-            toSwap = nextLevel
+            # print(s)
         
-        return time
+        return time - 1
         
         
