41bbff856991828fe0a3887d0d48cced375894b0
Amanuel Abiy <amanuelabiy.as@gmail.com>
2023-02-23 17:13:10 +0300

    Time: 31 ms (91.19%), Space: 14.1 MB (15.11%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
@@ -1,32 +1,28 @@
 class Solution:
-    def diffWaysToCompute(self, expression: str) -> List[int]:
-        """
-              |
-        "21-1-1"
-        """
-        memo = {}
-        op = {'+', '-', '*', '/'}
-        
-        def dp(i, j, memo):
-            if j - i + 1<= 2:
-                return [int(expression[i:j + 1])]
-            
-            if (i, j) in memo:
-                return memo[(i, j)]
+    def compute(self, a, b, op):
+        if op == "+":
+            return a + b
+        if op == "-":
+            return a - b
+        return a * b
 
-            ans = []
-            for x in range(i, j + 1):
-    
-                if expression[x] in op:
-                    left = dp(i, x - 1, memo)
-                    right = dp(x + 1, j, memo)
-                    for l in left:
-                        for r in right:
-                            v = eval(f"{l} {expression[x]} {r}")
-                            ans.append(v)
-                                            
-            return ans
+    @lru_cache(None)
+    def diffWaysToCompute(self, expression: str) -> List[int]:
+        if not expression:
+            return []
+        if expression.isdigit():
+            return [int(expression)]
         
-        return dp(0, len(expression) - 1, {})
+        ans = []
+        for ind, i in enumerate(expression):
+            if not i.isdigit():
+                a = self.diffWaysToCompute(expression[:ind])
+                b = self.diffWaysToCompute(expression[ind+1:])
+                
+                for x in a:
+                    for y in b:
+                        ans.append(self.compute(x, y, i))
             
+        
+        return ans
             
